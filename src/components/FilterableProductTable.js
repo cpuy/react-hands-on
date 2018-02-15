dd0479b525a2b30a4d90feb4f5789a3c3d573d00
Colin PUY <colin.puy@bonitasoft.com>
2018-02-15 14:05:49 +0100

    step4

--- src/components/FilterableProductTable.js
+++ src/components/FilterableProductTable.js
@@ -3,11 +3,26 @@ import SearchBar from './SearchBar';
 import ProductTable from './ProductTable';
 
 export default class FilterableProductTable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterText: '',
+      inStockOnly: false
+    };
+  }
+
   render() {
     return (
       <div>
-        <SearchBar />
-        <ProductTable products={this.props.products} />
+        <SearchBar
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+        />
+        <ProductTable
+          products={this.props.products}
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+        />
       </div>
     );
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
   render() {
+    const filterText = this.props.filterText;
+    const inStockOnly = this.props.inStockOnly;
+
     return (
       <form>
-        <input type="text" placeholder="Search..." />
+        <input
+          type="text"
+          placeholder="Search..."
+          value={filterText}
+        />
         <p>
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            checked={inStockOnly}
+          />
           {' '}
           Only show products in stock
         </p>
